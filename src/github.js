169f7f26d3062ed54a3f3256970e724b79b9b0be
James Moore <jamoor@microsoft.com>
2024-02-23 15:52:52 -0800

    ugh

--- src/github.js
+++ src/github.js
@@ -107,7 +107,7 @@ async function fetch_reviewers() {
   // Generated Octokit: https://github.com/octokit/plugin-rest-endpoint-methods.js/blob/main/src/generated/endpoints.ts
   // Timeline Rest APIs: https://docs.github.com/en/rest/issues/timeline?apiVersion=2022-11-28#about-timeline-events
   // Pagination: https://docs.github.com/en/rest/using-the-rest-api/using-pagination-in-the-rest-api?apiVersion=2022-11-28#example-using-the-octokitjs-pagination-method
-  const { data: response_body } = await octokit.paginate(octokit.rest.issues.listEventsForTimeline, {
+  const { data: response_body } = await octokit.paginate(octokit.issues.listEventsForTimeline, {
     owner: context.repo.owner,
     repo: context.repo.repo,
     issue_number: context.payload.pull_request.number,
