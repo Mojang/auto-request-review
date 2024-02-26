06194e94e3c20eee9b6f93ed922a19fe63637fe1
James Moore <jamoor@microsoft.com>
2024-02-26 14:33:37 -0800

    try again

--- src/github.js
+++ src/github.js
@@ -109,14 +109,14 @@ async function fetch_reviewers() {
   // Generated Octokit: https://github.com/octokit/plugin-rest-endpoint-methods.js/blob/main/src/generated/endpoints.ts
   // Timeline Rest APIs: https://docs.github.com/en/rest/issues/timeline?apiVersion=2022-11-28#about-timeline-events
   // Pagination: https://docs.github.com/en/rest/using-the-rest-api/using-pagination-in-the-rest-api?apiVersion=2022-11-28#example-using-the-octokitjs-pagination-method
-  const response2 = await octokit.paginate('GET /repos/{owner}/{repo}/issues/{issue_number}/timeline', {
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    issue_number: context.payload.pull_request.number,
-    per_page: per_page,
-  });
-  core.info('Paginate request format response');
-  core.info(JSON.stringify(response2));
+  // const response2 = await octokit.paginate('GET /repos/{owner}/{repo}/issues/{issue_number}/timeline', {
+  //   owner: context.repo.owner,
+  //   repo: context.repo.repo,
+  //   issue_number: context.payload.pull_request.number,
+  //   per_page: per_page,
+  // });
+  // core.info('Paginate request format response');
+  // core.info(JSON.stringify(response2));
 
   const response3 = await octokit.graphql.paginate(
     `
@@ -125,10 +125,8 @@ async function fetch_reviewers() {
           pullRequest(number: $number) {
               timelineItems(first: $per_page, after: $endCursor, itemTypes: REVIEW_REQUESTED_EVENT) {
                   nodes {
-                      __typename
                       ... on ReviewRequestedEvent {
                           requestedReviewer {
-                            __typename
                             ... on User {
                                   login
                             }
